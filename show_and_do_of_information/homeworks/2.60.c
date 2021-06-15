b898a3fa7c61c8851261796686294399a070203e
Whisper <72680955+Picky02-ji@users.noreply.github.com>
2021-06-15 14:33:30 +0800

    Update 2.60.c

--- show_and_do_of_information/homeworks/2.60.c
+++ show_and_do_of_information/homeworks/2.60.c
@@ -1,4 +1,3 @@
-~~~
 #include <stdio.h>
 unsigned replace_byte(unsigned x, unsigned char b, int i)
 {
@@ -17,9 +16,10 @@ int main(){
 	printf("%x",b);
 	return 0;
 }
-~~~
+/*
 将四个字节(假设情况)中的其中一个替换为其他值，只需把其先变为0再赋值。  
 shift_left是左移位数，(w&i)限制i的范围，<<3把字节位转化为比特位；   
 故shift_left表示i位。
 
 参考代码:[](https://github.com/vonzhou/CSAPP/blob/master/exercise/ex2-60.c)
+*/
